1cc9070cd9bd1bdd8a1c8bb3bce2afdc6b995a69
Peter Turczak <peter@turczak.de>
2018-01-07 20:19:00 +0100

    Mostly hacks to crash less, no better sound, yet

--- pywizard/Preview.py
+++ pywizard/Preview.py
@@ -11,6 +11,7 @@ import logging
 
 class Preview(object):
     chirp = (0x00,0x2a,0xd4,0x32,0xb2,0x12,0x25,0x14,0x02,0xe1,0xc5,0x02,0x5f,0x5a,0x05,0x0f,0x26,0xfc,0xa5,0xa5,0xd6,0xdd,0xdc,0xfc,0x25,0x2b,0x22,0x21,0x0f,0xff,0xf8,0xee,0xed,0xef,0xf7,0xf6,0xfa,0x00,0x03,0x02,0x01)
+    chirp = ( 0x00, 0x03, 0x0f, 0x28, 0x4c, 0x6c, 0x71, 0x50, 0x25, 0x26, 0x4c, 0x44, 0x1a, 0x32, 0x3b, 0x13, 0x37, 0x1a, 0x25, 0x1f, 0x1d, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)
 
     def __init__(self, frames):
         self.frames = frames[:]
@@ -29,6 +30,8 @@ class Preview(object):
         lattice_fwd = [0.] * 11
         lattice_rev = [0.] * 11
 
+        #fix_chirp_data = lambda x: (x<<6) if ( (x<<6) & 0x8000 ) == 0 else int(0x100000-(x<<6))
+        fix_chirp_data = lambda x: x
         while decoding:
             cur_frame = self.frames.pop()
             pitch_count = 0
@@ -42,24 +45,23 @@ class Preview(object):
 
             frame_state = cur_frame.translatedParameters()
 
-            if cur_frame.reflector.isVoiced:
-                in_data = [ (x-128.)/128. * frame_state['kParameterGain']/(2**13) for x in self.chirp ]
+            if cur_frame.reflector.isVoiced and 'kParameterPitch' in frame_state:
+                in_data = [ fix_chirp_data(x) * frame_state['kParameterGain']/(2**13) for x in self.chirp ]
                 period = frame_state['kParameterPitch']
-                print "a", frame_state['kParameterGain']
+                print "a", frame_state['kParameterGain'], period
             else:
                 period = 200
-                in_data = ( scipy.rand(200)* frame_state['kParameterGain'])/(2.**13) .tolist()
+                in_data = ( sp.rand(200)* frame_state['kParameterGain']/(2.**13)) .tolist()
                 print "b"
                 #for x in range(100):
                 #    rng = (rng >> 1) ^ ( 0xB800 if (rng &1) else 0)
                 #    in_data.append(rng)
             print max(in_data), min(in_data)
 
-            if frame_state['kParameterGain'] == 0:
+            if frame_state['kParameterGain'] == 0 or frame_state['kParameterPitch'] == 0:
                 #FIXME: What now?!
-                #continue
-                pass
-
+                continue
+                #pass
 
             if not frame_state['kParameterRepeat']:
                 # Repeat frames keep last reflector bits
@@ -82,7 +84,9 @@ class Preview(object):
                 pos = i % int(period)
                 if pos < len(in_data):
                     value = in_data[ pos ]
+                    print pos
                 else:
+                    print "FIN"
                     value = 0.
 
                 lattice_fwd[10] = value
@@ -101,7 +105,7 @@ class Preview(object):
                     # 1/0: / 32768 !!
                 lattice_rev[0] = lattice_fwd[0]
 
-                yield lattice_fwd[0] / 4.
+                yield lattice_fwd[0] / float(1<<5)
 
     def process(self):
         return(self.frameData)
